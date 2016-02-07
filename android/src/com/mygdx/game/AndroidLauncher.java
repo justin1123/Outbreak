a64053766ae1c32909719d7d63c1570b41fe706d
Justin <johnson.justin.john@gmail.com>
2016-02-07 13:55:03 -0500

    first commit

---
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.game.OutbreakGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new OutbreakGame(), config);
+	}
+}
